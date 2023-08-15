097698637c88bb81a34d58c1199c99a4230c217c
psimarro <psimarro@c1r10s5.42madrid.com>
2023-08-15 10:46:19 +0200

    Creado makefile, libft añadida,  creada estructura preliminar

---
+++ inc/minishell.h
@@ -0,0 +1,30 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+
+typedef struct s_cmdlist
+{
+	struct s_cmdlist    *next;
+	struct s_cmdlist	*pre;
+	char                *cmd;
+    char                **flags;
+    char                **args;
+}	t_cmdlist;
+
+typedef struct s_mshell
+{
+	t_cmdlist	*cmds;
+	int			pipex;	//un solo comando o pipex
+	int			env; 	//variable de entorno $?
+}	t_mshell;
+
+#endif
