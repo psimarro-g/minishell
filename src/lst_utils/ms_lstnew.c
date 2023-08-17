6167de5f24949603b60c7aff747d1e8d5a3a9db3
David Montoro couso <dmontoro@c1r12s4.42madrid.com>
2023-08-17 09:30:26 +0200

    Creada la funcion lstlast

--- src/lst_utils/ms_lstnew.c
+++ src/lst_utils/ms_lstnew.c
@@ -1,22 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ms_lstlast.c                                       :+:      :+:    :+:   */
+/*   ms_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmontoro <dmontoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/17 09:20:09 by dmontoro          #+#    #+#             */
-/*   Updated: 2023/08/17 09:28:12 by dmontoro         ###   ########.fr       */
+/*   Created: 2023/08/17 09:28:41 by dmontoro          #+#    #+#             */
+/*   Updated: 2023/08/17 09:28:41 by dmontoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../inc/ms_lst.h"
-
-t_cmdlist	*ms_lstlast(t_cmdlist *lst)
-{
-	if (!lst)
-		return (0);
-	while (lst && lst->next)
-		lst = lst->next;
-	return (lst);
-}
