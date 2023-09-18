8c658e1241ded4203147e557ca672829e94f00e1
Souls <dmonto01@ucm.es>
2023-09-18 12:20:04 +0200

    Fixed: Now if a env variable doesnt have a value, it only shows it in the export and not in env

--- src/built_ins/env.c
+++ src/built_ins/env.c
@@ -6,12 +6,13 @@
 /*   By: dmontoro <dmontoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 07:26:02 by dmontoro          #+#    #+#             */
-/*   Updated: 2023/09/06 10:00:07 by dmontoro         ###   ########.fr       */
+/*   Updated: 2023/09/18 12:19:03 by dmontoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
+//si strchr no encontrase el caracter, esto petaria, pero siempre lo va a encontrar porque es una variable de entorno
 int	env(char **envp)
 {
 	int	i;
@@ -19,7 +20,8 @@ int	env(char **envp)
 	i = 0;
 	while (envp[i] != NULL)
 	{
-		printf("%s\n", envp[i]);
+		if (ft_strchr(envp[i], '=')[1] != '\0')
+			printf("%s\n", envp[i]);
 		i++;
 	}
 	return (0);
